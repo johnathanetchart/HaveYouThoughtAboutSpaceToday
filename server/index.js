7d666373102859395636844850992971217cd59e
Johnathan Etchart <johnathan.etchart@gmail.com>
2021-03-30 13:31:34 -0700

    finally got webpack going

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`)
