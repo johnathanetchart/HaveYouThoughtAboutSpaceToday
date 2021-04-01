9403ba95fda147687e0ceed186f637794de2b9c8
Johnathan Etchart <johnathan.etchart@gmail.com>
2021-04-01 08:26:19 -0700

    added watchlist component

---
+++ client/src/components/WatchList.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import NeoCard from './NeoCard';
+
+class WatchList extends React.Component {
+  constructor(props) {
+    super (props);
+
+
+  }
+
+  render () {
+    // const { numberOfNeos, numberOfHazards, nearEarthObjects } = this.props;
+    // if (nearEarthObjects.length > 0) {
+    //   return (
+    //     <div
+    //       id="neoList"
+    //     >
+    //       On this day, there are / will be / were { numberOfNeos } Near Earth Objects, {numberOfHazards} of which NASA classifies as potentially hazardous.
+    //       {
+    //         nearEarthObjects.map((neo) => {
+    //           return (
+    //             <NeoCard neo={neo} />
+    //           )
+    //         })
+    //       }
+    //     </div>
+    //   )
+    // } else {
+      return null;
+    // }
+  }
+
+}
+
+export default WatchList;
